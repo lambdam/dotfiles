4f63eef1a3d87d6416dd268597f4decf90305c4d
Damien Ragoucy <damien.ragoucy@af83.com>
2014-02-16 15:00:02 +0100

    Updated emacs init

--- emacs.d/custom.el
+++ emacs.d/custom.el
@@ -4,8 +4,8 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(ansi-color-names-vector ["black" "#d55e00" "#009e73" "#f8ec59" "#0072b2" "#cc79a7" "#56b4e9" "white"])
- '(custom-enabled-themes (quote (wheatgrass)))
- '(custom-safe-themes (quote ("c6a6b4d59dc53d407432e4f936c49bf747e304683cc357abe32398af7c6dea33" "73fe242ddbaf2b985689e6ec12e29fab2ecd59f765453ad0e93bc502e6e478d6" "0e121ff9bef6937edad8dfcff7d88ac9219b5b4f1570fd1702e546a80dba0832" default)))
+ '(custom-enabled-themes (quote (monokai)))
+ '(custom-safe-themes (quote ("7f2b040545a8795c441aaab8592dc82219be5c545394aa44dac5a3ae551d5f09" "c6a6b4d59dc53d407432e4f936c49bf747e304683cc357abe32398af7c6dea33" "73fe242ddbaf2b985689e6ec12e29fab2ecd59f765453ad0e93bc502e6e478d6" "0e121ff9bef6937edad8dfcff7d88ac9219b5b4f1570fd1702e546a80dba0832" default)))
  '(package-archives (quote (("gnu" . "http://elpa.gnu.org/packages/") ("melpa" . "http://melpa.milkbox.net/packages/") ("maramlade" . "http://marmalade-repo.org/packages/")))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,5 +1,5 @@
 ;; Turn off mouse interface early in startup to avoid momentary display
-;; (if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
+(if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
 (if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
 (if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
 
@@ -13,4 +13,12 @@
 (setq custom-file (expand-file-name "custom.el" user-emacs-directory))
 (load custom-file)
 
-;; (load-theme 'molokai t)
+;; Display line number
+(global-linum-mode t)
+(setq linum-format "%d")
+
+;; Evil mode
+(evil-mode 1)
+
+;; Rainbow delimiters
+(global-rainbow-delimiters-mode)
