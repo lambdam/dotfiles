4d29ba034c1dea881824256d16d7d901a9a34c5d
Damien Ragoucy <damien.ragoucy@af83.com>
2014-02-27 16:36:46 +0100

    Updated emacs.d

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -6,6 +6,9 @@
 ;; No splash screen please
 (setq inhibit-startup-message t)
 
+;; Auto revert (~ reload modified files)
+(global-auto-revert-mode t)
+
 (require 'package)
 (package-initialize)
 
@@ -14,11 +17,39 @@
 (load custom-file)
 
 ;; Display line number
-(global-linum-mode t)
-(setq linum-format "%d")
+;; (global-linum-mode t)
+;; (setq linum-format "%d")
+
+;; Display line number
+(setq column-number-mode t)
 
 ;; Evil mode
 (evil-mode 1)
 
 ;; Rainbow delimiters
 (global-rainbow-delimiters-mode)
+
+;; 
+(load-library "iso-transl")
+
+;; For Mac OS rebind meta to function key
+(when (eq system-type 'darwin)
+  (setq mac-function-modifier 'meta)
+  (setq mac-option-modifier 'none))
+
+;; store all backup and autosave files in the tmp dir
+(setq backup-directory-alist
+      `((".*" . ,temporary-file-directory)))
+(setq auto-save-file-name-transforms
+      `((".*" ,temporary-file-directory t)))
+
+;; IDO
+(ido-mode t)
+(setq ido-enable-flex-matching t
+      ido-use-virtual-buffers t)
+
+;; SMEX
+(setq smex-save-file (expand-file-name ".smex-items" user-emacs-directory))
+(smex-initialize)
+(global-set-key (kbd "M-x") 'smex)
+(global-set-key (kbd "M-X") 'smex-major-mode-commands)
