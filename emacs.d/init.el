19e0870df83d665c4da9da15f8bf1fa8fdafbd14
Damien Ragoucy <damien.ragoucy@af83.com>
2014-03-13 13:32:31 +0100

    Updated emacs files

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -18,7 +18,7 @@
 
 ;; Display line number
 (global-linum-mode t)
-(setq linum-format "%d")
+;; (setq linum-format "%d")
 ;; (setq linum-format "%4d \u2502 ")
 
 ;; Display line number
@@ -30,7 +30,7 @@
 ;; Rainbow delimiters
 (global-rainbow-delimiters-mode)
 
-;; 
+;;
 (load-library "iso-transl")
 
 ;; For Mac OS rebind meta to function key
@@ -45,9 +45,13 @@
       `((".*" ,temporary-file-directory t)))
 
 ;; IDO
-(ido-mode t)
+(require 'ido)
+(require 'ido-ubiquitous)
+(require 'flx-ido)
 (setq ido-enable-flex-matching t
       ido-use-virtual-buffers t)
+(ido-mode +1)
+(ido-ubiquitous-mode +1)
 
 ;; SMEX
 (setq smex-save-file (expand-file-name ".smex-items" user-emacs-directory))
@@ -56,14 +60,45 @@
 (global-set-key (kbd "M-X") 'smex-major-mode-commands)
 
 ;; emacs-smooth-scrolling.el
-(setq scroll-step 1)
-(setq scroll-conservatively 1000)
-(setq mouse-wheel-scroll-amount '(1 ((shift) . 1)))
-(setq mouse-wheel-progressive-speed nil)
-(setq mouse-wheel-follow-mouse t)
+(require 'smooth-scrolling)
+;; (setq scroll-step 1)
+;; (setq scroll-conservatively 1000)
+;; (setq mouse-wheel-scroll-amount '(1 ((shift) . 1)))
+;; (setq mouse-wheel-progressive-speed nil)
+;; (setq mouse-wheel-follow-mouse t)
+
+;; smooth-scroll
+(require 'smooth-scroll)
+(smooth-scroll-mode 'toggle)
 
 ;; disable ring bell
 (setq ring-bell-function 'ignore)
 
 ;; Autocomplete
+(require 'auto-complete)
+(ac-flyspell-workaround)
+(ac-linum-workaround)
 (global-auto-complete-mode t)
+
+;; Multiple cursors
+(require 'multiple-cursors)
+
+;; Dired
+(toggle-diredp-find-file-reuse-dir 1)
+
+;; Font size
+(set-face-attribute 'default nil :height 130)
+
+;; Multiple cursors
+;; (setq multiple-cursors-map (make-sparse-keymap))
+;; (define-key multiple-cursors-map (kbd "C->") 'mc/mark-next-like-this)
+;; (add-hook
+;;  'multiple-cursors-mode-hook
+;;  (lambda ()
+;;    ))
+
+;; Nice scrolling
+(setq
+ scroll-margin 0
+ scroll-conservatively 100000
+ scroll-preserve-screen-position 1)
