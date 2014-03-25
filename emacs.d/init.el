3c5607424ed8691a72c86915e24f261818515553
Damien Ragoucy <damien.ragoucy@af83.com>
2014-03-25 21:09:42 +0100

    Emacs: hide menu bar

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -55,7 +55,7 @@
 (load custom-file)
 
 ;; Turn off mouse interface early in startup to avoid momentary display
-;; (if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
+(if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
 (if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
 (if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
 
