bbb8c77744183f7bd049669b8aad3602d00d1533
Damien Ragoucy <damien.ragoucy@af83.com>
2014-03-25 21:22:17 +0100

    Added Ubuntu init script

---
+++ _init/ubuntu.sh
@@ -0,0 +1,58 @@
+PACKAGES="\
+autoconf \
+bison \
+build-essential \
+curl \
+git \
+gitg \
+emacs24 \
+libxslt1-dev \
+libreadline6 \
+libreadline6-dev \
+libssl-dev \
+libxml2-dev \
+libxslt1-dev \
+libyaml-dev \
+nginx-full \
+oracle-java7-installer \
+terminator \
+vim \
+wmctrl \
+xchat-gnome \
+zlib1g \
+zlib1g-dev \
+"
+
+RUBY_VERSION="2.1.1"
+
+NODE_VERSION="0.11.12"
+
+##########
+
+# PPA
+sudo add-apt-repository ppa:webupd8team/java
+
+# Refresh
+sudo apt-get update
+sudo apt-get upgrade
+
+# Install all packages
+sudo apt-get install $PACKAGES
+
+# Ruby
+git clone https://github.com/sstephenson/rbenv.git ~/.rbenv
+git clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
+rbenv install $RUBY_VERSION
+rbenv rehash
+rbenv global $RUBY_VERSION
+gem install bundler
+
+# VIM
+mkdir -p ~/.vim/bundle
+git clone https://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
+
+# Node
+git clone https://github.com/creationix/nvm.git ~/.nvm
+source ~/.nvm/nvm.sh
+nvm install $NODE_VERSION
+nvm alias default $NODE_VERSION
