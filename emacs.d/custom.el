1d3643b8c66732155cdc079c31d55b1042ab58a1
Damien Ragoucy <damien.ragoucy@af83.com>
2014-09-02 17:18:57 +0200

    Emacs > customize: automatic indentation

--- emacs.d/custom.el
+++ emacs.d/custom.el
@@ -4,10 +4,13 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(ac-use-fuzzy t)
- '(ansi-color-names-vector ["black" "#d55e00" "#009e73" "#f8ec59" "#0072b2" "#cc79a7" "#56b4e9" "white"])
+ '(ansi-color-names-vector
+   ["black" "#d55e00" "#009e73" "#f8ec59" "#0072b2" "#cc79a7" "#56b4e9" "white"])
  '(css-indent-offset 2)
  '(custom-enabled-themes (quote (monokai)))
- '(custom-safe-themes (quote ("a0be8cc2d04108146aa14c493071b88578157a11bd2ef0d85e3ca34b5f2205c5" "338f622cba1f52523b57dddbb6ec255196ba1dbfce490aa2fed5ce0b038ce9af" "7f2b040545a8795c441aaab8592dc82219be5c545394aa44dac5a3ae551d5f09" "c6a6b4d59dc53d407432e4f936c49bf747e304683cc357abe32398af7c6dea33" "73fe242ddbaf2b985689e6ec12e29fab2ecd59f765453ad0e93bc502e6e478d6" "0e121ff9bef6937edad8dfcff7d88ac9219b5b4f1570fd1702e546a80dba0832" default)))
+ '(custom-safe-themes
+   (quote
+    ("776e35f5a92988a39582ca20b528a98685bf6d441950d5363254705697068fb4" "bd88c659ca050475ae773848adb29720b99c2983450cf21cab6b357d9b1a40e5" "690a0889f56a53c9e4137a3a6c43a164030f67a675486ba592c7b1a02d4c518e" "bd115791a5ac6058164193164fd1245ac9dc97207783eae036f0bfc9ad9670e0" "a0be8cc2d04108146aa14c493071b88578157a11bd2ef0d85e3ca34b5f2205c5" "338f622cba1f52523b57dddbb6ec255196ba1dbfce490aa2fed5ce0b038ce9af" "7f2b040545a8795c441aaab8592dc82219be5c545394aa44dac5a3ae551d5f09" "c6a6b4d59dc53d407432e4f936c49bf747e304683cc357abe32398af7c6dea33" "73fe242ddbaf2b985689e6ec12e29fab2ecd59f765453ad0e93bc502e6e478d6" "0e121ff9bef6937edad8dfcff7d88ac9219b5b4f1570fd1702e546a80dba0832" default)))
  '(ecb-layout-name "left13")
  '(ecb-options-version "2.40")
  '(ecb-windows-width 0.15)
@@ -21,8 +24,12 @@
  '(js-indent-level 2)
  '(linum-eager t)
  '(linum-format (quote dynamic))
- '(package-archives (quote (("gnu" . "http://elpa.gnu.org/packages/") ("melpa" . "http://melpa.milkbox.net/packages/") ("maramlade" . "http://marmalade-repo.org/packages/"))))
  '(neo-window-width 35)
+ '(package-archives
+   (quote
+    (("gnu" . "http://elpa.gnu.org/packages/")
+     ("melpa" . "http://melpa.milkbox.net/packages/")
+     ("maramlade" . "http://marmalade-repo.org/packages/"))))
  '(projectile-global-mode t)
  '(smooth-scroll/vscroll-step-size 8)
  '(sr-speedbar-right-side nil)
