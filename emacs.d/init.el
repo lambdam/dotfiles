70249400ef37c1b7966f7f8ff0630a620beb3789
Damien Ragoucy <damien.ragoucy@af83.com>
2014-10-12 18:16:03 +0200

    Emacs: Haskell mode

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -11,6 +11,7 @@
     flx
     flx-ido
     fuzzy
+    haskell-mode
     helm
     highlight-sexp
     ido-ubiquitous
@@ -174,6 +175,35 @@
 (global-set-key (kbd "<M-S-right>") 'windmove-right)
 (global-set-key (kbd "<M-S-up>") 'windmove-up)
 (global-set-key (kbd "<M-S-down>") 'windmove-down)
+
+
+;; Language modes
+;; ==============
+
+;; Haskell
+;; -------
+
+(add-hook 'haskell-mode-hook 'interactive-haskell-mode)
+(custom-set-variables
+  '(haskell-process-suggest-remove-import-lines t)
+  '(haskell-process-auto-import-loaded-modules t)
+  '(haskell-process-log t))
+
+;; Haskell bindings
+(define-key haskell-mode-map (kbd "C-c C-l") 'haskell-process-load-or-reload)
+(define-key haskell-mode-map (kbd "C-`") 'haskell-interactive-bring)
+(define-key haskell-mode-map (kbd "C-c C-t") 'haskell-process-do-type)
+(define-key haskell-mode-map (kbd "C-c C-i") 'haskell-process-do-info)
+(define-key haskell-mode-map (kbd "C-c C-c") 'haskell-process-cabal-build)
+(define-key haskell-mode-map (kbd "C-c C-k") 'haskell-interactive-mode-clear)
+(define-key haskell-mode-map (kbd "C-c c") 'haskell-process-cabal)
+(define-key haskell-mode-map (kbd "SPC") 'haskell-mode-contextual-space)
+
+;; Cabal bindings
+(define-key haskell-cabal-mode-map (kbd "C-`") 'haskell-interactive-bring)
+(define-key haskell-cabal-mode-map (kbd "C-c C-k") 'haskell-interactive-mode-clear)
+(define-key haskell-cabal-mode-map (kbd "C-c C-c") 'haskell-process-cabal-build)
+(define-key haskell-cabal-mode-map (kbd "C-c c") 'haskell-process-cabal)
 
 ;; Web
 ;; ---
