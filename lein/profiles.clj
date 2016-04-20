c8d7449982ae4d66e89921514192e30909731ae5
Damien Ragoucy <damien@praditus.com>
2016-04-20 09:24:43 -0500

    Update lein profiles

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,1 +1,13 @@
-{:user {:plugins [[cider/cider-nrepl "0.8.2"]]}}
+{:user {:plugins [;; http://blog.mattgauger.com/blog/2014/09/15/clojure-code-quality-tools/
+                  [lein-ancient "0.6.8"]
+                  [lein-kibit "0.1.2"]
+                  [jonase/eastwood "0.2.3"]
+                  [lein-bikeshed "0.2.0"]
+                  [slamhound "1.5.5"]
+                  [lein-cloverage "1.0.6"]
+                  ;; ---
+                  #_[hiccup-bridge "1.0.1"]
+                  ]
+        ;; :aliases {"slamhound" ["run" "-m" "slam.hound"]}
+        ;; :dependencies [[org.clojure/clojure "1.7.0-beta2"]]
+        }}
