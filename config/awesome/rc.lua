b5472ef15ced1e5812c3bf4352edba4435a32cf5
Damien Ragoucy <dam@ramdam.io>
2018-11-12 10:31:27 +0100

    Update Awesome config

--- config/awesome/rc.lua
+++ config/awesome/rc.lua
@@ -74,6 +74,7 @@ awful.layout.layouts = {
     -- awful.layout.suit.spiral,
     -- awful.layout.suit.spiral.dwindle,
     awful.layout.suit.max,
+    awful.layout.suit.tile.top, -- copied from above
     -- awful.layout.suit.max.fullscreen,
     awful.layout.suit.magnifier,
     -- awful.layout.suit.corner.nw,
